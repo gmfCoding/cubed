68dcb2fdc096f22f5771a96626ed74f70b48ff7b
Christopher Lovell <clovell@student.42adel.org.au>
2023-11-19 02:33:16 +1030

    Update asset refresh tool Add example model

--- tools/refresh.sh
+++ tools/refresh.sh
@@ -13,11 +13,32 @@ if [[ $? -ne 0 ]]; then
 	pip3 install xpm
 fi
 
-FILES=$(find ../assets -type f -name "*.png" )
+OWD=$(pwd)
+cd $(dirname $(realpath $0))
 
+BGREEN="\033[1;32m"
+GREEN="\033[0;32m"
+BRED="\033[1;31m"
+RED="\033[0;31m"
+BYELLOW="\033[1;33m"
+YELLOW="\033[0;33m"
+BBLUE="\033[1;34m"
+BLUE="\033[0;34m"
+BCYAN="\033[1;36m"
+CYAN="\033[0;36m"
+NC="\033[0m"
+
+FILES=$(find ../assets/source/ -type f -name "*.png" )
 for filename in $FILES; do
     IN=$(realpath -m $filename) # png path
-	OUT=$(realpath -m $(echo $filename | sed s,/assets/png/,/assets/,g | sed s,.png,.xpm,g)) # converts png path to xpm path
+	OUT=$(realpath -m $(echo $filename | sed s,/assets/source/,/assets/,g | sed s,.png,.xpm,g)) # converts png path to xpm path
 	mkdir -p $(dirname $OUT)
+	printf ""$GREEN"converting $BLUE$IN$NC to$RED $OUT\n$NC"
     python3 -m xpm $IN > $OUT
 done
+
+cd $OWD
+
+# TODO automatic asset pipeline refresh?
+# BLENDER=/mnt/c/Program Files/Blender Foundation/Blender 3.6/blender-launcher.exe
+# $BLENDER -b assets/source/models/cube.blend -x 1 -o //render -f 0 # automatically rerender blend file
