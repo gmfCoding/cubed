f1b2349462f477323d32566a445303ea7e053307
kyle <kylemordaunt@gmail.com>
2023-11-23 13:21:18 +1030

    not much progress here added a function to replace tabs with spaces based on the defined size of tab also checking for map condition type and skipping over them for now will also have to check for empty lines on invalid chars

--- src/map_parser/map_convert.c
+++ src/map_parser/map_convert.c
@@ -17,9 +17,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-
+#define TAB_SIZE 4 
 #define MAP_MAX_XY 200
 #define MAP_MAX_X MAP_MAX_XY
 #define MAP_MAX_Y MAP_MAX_XY
@@ -57,7 +59,10 @@ typedef struct	s_map
 	float		s_angle;
 	int			color_ceiling;
 	int			color_floor;
-	char*		
+	char*           texture_n_wall;
+        char*           texture_s_wall;
+        char*           texture_w_wall;
+        char*           texture_e_wall;
 
 }				t_map;
 
@@ -69,6 +74,15 @@ typedef struct	s_world
 	//int ent_count;
 }				t_world;
 
+
+typedef enum	e_mapissue
+{
+		MAP_VALID,
+		MAP_INVALID_CHAR,
+		MAP_NOSTART,
+		MAP_MISSINGTEXTURE,
+		MAP_BADFILETYPE,
+}		t_mapissue;
 
 t_list	*ft_lst_readfile(const char *path)
 {
@@ -95,23 +109,123 @@ t_list	*ft_lst_readfile(const char *path)
 	return (first);
 }
 
+char *const    g_mapsymbols[] = {
+	"NO",
+	"SO",
+	"WE",
+	"EA",
+	"S ",
+	"F ",
+	"C ",
+	NULL
+};
+//if(ft_strcmp(".cub", ft_strrchr(filePath, ".")) == 0)
+
+int	map_starting_point(char *content)
+{
+	int	i;
+	char	temp[3];
+
+	i = -1;
+	strncpy(temp, content, 2);
+	temp[2] = '\0';
+	while (g_mapsymbols[++i] != NULL)
+	{
+		if (strcmp(g_mapsymbols[i], temp) == 0)
+			return (1);
+	//	printf("%s\n", g_mapsymbols[i]);
+	}
+	return (0);
+}
+
+void	replace_tabs(t_list *curr)
+{
+	int	i;
+	int	j;
+	char	*line;
+
+	while (curr != NULL && curr->content != NULL)
+	{
+		line = (char *)curr->content;
+		i = 0;
+		while (line[i] != '\0')
+		{
+			if (line[i] == '\t')
+			{
+				memmove(&line[i + TAB_SIZE], &line[i + 1], strlen(&line[i + 1]) + 1);
+				j = 0;
+				while (j < TAB_SIZE)
+					line[i + j++] = ' ';
+				i += TAB_SIZE;
+			}
+			else
+				i++;
+		}
+		curr = curr->next;
+	}
+}
+
+int	map_width_size(t_list *curr)
+{
+	int i;
+
+	i = 0;
+	while (curr != NULL && curr->content != NULL)
+	{
+		if (strlen((char *)curr->content) > i)
+			i = strlen((char *)curr->content);
+		curr = curr->next;
+	}
+
+	return (i);
+}
+
+t_map	map_init(t_map map, t_list *raw_map_file)
+{
+	t_list	*curr;
+	
+	curr = raw_map_file;
+	int lines = (intptr_t)curr->content;
+	curr = curr->next;
+	while (curr != NULL && map_starting_point((char *)curr->content) == 1)
+		curr = curr->next;
+	replace_tabs(curr);
+	map.width = map_width_size(curr);
+	printf("%d\n", map.width);
+	while (curr != NULL && curr->content != NULL)
+	{
+		//populatemap
+		printf("%s", (char *)curr->content);
+		curr = curr->next;
+			
+	}
+
+	
+	return (map);
+}
+
+t_map	map_parse(int argc, char **argv)//get the map done first
+{
+	t_map	map;
+	t_list	*raw_map_file;
+
+	if (argc == 1)
+		raw_map_file = ft_lst_readfile("map1.cub");
+	else if (argc == 2)
+		raw_map_file = ft_lst_readfile(argv[1]);
+	else
+		exit(2);
+	map = map_init(map, raw_map_file);
+
+
+	return (map);
+}
+
 t_world	world_preset(int argc, char **argv)
 {
 	t_world	world;
-	t_list	*raw_map_file;
-	t_list	*curr;
-	raw_map_file = ft_lst_readfile("map1.cub");
-	curr = raw_map_file;
-	if (curr == NULL)
-		return ;
-	int lines = (intptr_t)curr->content;
-		curr = curr->next;
-	while (curr != NULL && curr->content != NULL)
-	{
 
-		printf("%s", (char *)curr->content);
-		curr = curr->next;
-	}
+	world.map = map_parse(argc, argv);
 
 	return (world);
 }
