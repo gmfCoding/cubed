651307b9363c1f54c064cf9994f98551f902b640
kyle <kylemordaunt@gmail.com>
2023-11-27 00:56:17 +1030

    map checking done started setting up player pos and player rotation still more setup for the player can be done in the function started working on map modifier adding texture and color based of function pointer. still got a bit to go

--- src/map_parser/map_convert.c
+++ src/map_parser/map_convert.c
@@ -19,9 +19,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+#include <errno.h>
 
 
-#define TAB_SIZE 4 
+#define TAB_SIZE 4
+#define	FILE_TYPE ".txt"
+#define MAP_MODIFICATION_SIZE 100
 #define MAP_MAX_XY 200
 #define MAP_MAX_X MAP_MAX_XY
 #define MAP_MAX_Y MAP_MAX_XY
@@ -46,14 +50,34 @@ typedef enum	e_tiletype
 	INVALID
 }				t_tiletype;
 
+
+
+typedef enum	e_modtype
+{
+	NORTH_TEXTURE,
+	SOUTH_TEXTURE,
+	WEST_TEXTURE,
+	EAST_TEXTURE,
+	FLOOR_COLOR,
+	CEILING_COLOR,
+}				t_modtype;
+
+
 typedef struct	s_tile
 {
 	t_tiletype	type; // 1 or 0?
 }				t_tile;
 
+typedef struct	s_mod
+{
+	t_modtype	type;
+	char*		content;
+}			t_mod;
+
 typedef struct	s_map
 {
 	t_tile	tiles[MAP_MAX_X * MAP_MAX_Y];
+	t_mod	mods[MAP_MODIFICATION_SIZE];
 	t_vec2		s_pos;
 	uint8_t		width;
 	uint8_t		height;
@@ -66,9 +90,20 @@ typedef struct	s_map
 	char*		texture_e_wall;
 }				t_map;
 
+typedef struct	s_player
+{
+	t_vec2	pos;
+	t_vec2	dir;
+	t_vec2	plane;
+
+	double	moveSpeed;
+	double	rotSpeed;
+}		t_player;
+
 typedef struct	s_world
 {
-	t_map map;
+	t_map		map;
+	t_player	player;
 
 	//t_entity	map_entities[MAX_ENTITIES];
 	//int ent_count;
@@ -82,6 +117,31 @@ typedef enum	e_mapissue
 		MAP_MISSINGTEXTURE,
 		MAP_BADFILETYPE,
 }		t_mapissue;
+
+char *const    g_mapsymbols[] = {
+	"NO",
+	"SO",
+	"WE",
+	"EA",
+	"S ",
+	"F ",
+	"C ",
+};
+
+int    ft_lstremove(t_list **lst, t_list *target, void (*del)(void*))
+{
+	while (*lst != NULL)
+	{
+		if (*lst == target)
+		{
+			*lst = target->next;
+			ft_lstdelone(target, del);
+			return (1);
+		}
+		lst = &(*lst)->next;
+	}
+	return (0);
+}
 
 t_list	*ft_lst_readfile(const char *path)
 {
@@ -108,17 +168,31 @@ t_list	*ft_lst_readfile(const char *path)
 	return (first);
 }
 
-char *const    g_mapsymbols[] = {
-	"NO",
-	"SO",
-	"WE",
-	"EA",
-	"S ",
-	"F ",
-	"C ",
-};
+int	ft_isspace(int c)
+{
+	c = (unsigned char)c;
+	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	return (0);
+}
 
-int	map_starting_point(char *content)
+int	is_empty_line(const char *line)
+{
+	int	i;
+	
+	if(!line)
+		return (1);
+	i = 0;
+	while (line[i])
+	{
+		if (!ft_isspace(line[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	map_skip_over_modifiers(char *content)
 {
 	int	i;
 	char	temp[3];
@@ -135,6 +209,20 @@ int	map_starting_point(char *content)
 	}
 	return (0);
 }
+
+int	map_starting_tile(char *content)
+{
+	int	i;
+	
+//	if (!content)
+	i = 0;
+	while (ft_isspace(content[i]))
+		i++;
+	if(content[i] == '1')
+		return (1);
+	return (0);
+}
+
 
 void	replace_tabs(t_list *curr)
 {
@@ -225,7 +313,7 @@ int	map_tiles(t_map *map, char *content, int index)
 	return (i);
 }
 
-void	print_map(t_map *map)
+void	map_print(t_map *map)
 {
 	int	i;
 
@@ -314,6 +402,81 @@ int	map_check_surrounded(t_map *map_temp, int pos)
 		return (1);
 	return (0);
 }
+
+int	map_check_element_colors(char *str)
+{
+	int	i;
+	int	j;
+	char	**rgb;
+	int	syntax_check;
+
+	i = 0;
+	syntax_check = 0;
+	while (*str == ' ')
+		str++;
+	rgb = ft_split(str, ',');
+	while(rgb[i])
+	{
+		if (atoi(rgb[i]) > 255 || atoi(rgb[i]) < 0 || is_empty_line(rgb[i]))
+			syntax_check = 1;
+		j = 0;
+		while (rgb[i][j] && rgb[i][j] != '\n' && rgb[i][j] != ' ')
+			if (!ft_isdigit(rgb[i][j++]))
+				syntax_check = 1;
+		free(rgb[i++]);
+	}
+	if (i != 3)
+		syntax_check = 1;
+	free(rgb);
+	return (syntax_check);
+}
+
+
+/* str cant have spaces on end currently unsure if i should add that or not */
+int	map_check_element_texture(char *str)
+{
+	int	texture_file;
+	int	syntax_check;
+	int	i;
+
+	i = 0;
+	syntax_check = 0;
+	str[ft_strlen(str) - 1] = '\0';
+	while (*str == ' ')
+		str++;
+	texture_file = open(str, O_RDONLY);
+	if (texture_file == -1)
+		syntax_check = 1;
+	if (strcmp(FILE_TYPE, ft_strrchr(str, '.')))
+		syntax_check = 1;
+	close(texture_file);
+	return (syntax_check);
+}
+
+int	map_check_elements(t_list *raw_map_files)
+{
+	t_list	*curr;
+	char	*str;
+
+	curr = raw_map_files;
+	while (curr != NULL && curr->content != NULL)
+	{
+		str = (char *)curr->content;
+		if (map_starting_tile(str) == 1)
+			break ;
+		if (str[2] && str[1] != ' ' && str[2] != ' ')
+			return (1);
+		if (str[1] && (str[0] == 'C' || str[0] == 'F') && str[1] == ' ')
+			if (map_check_element_colors(str + 2) == 1)
+				return (1);
+		if (str[2] && !strncmp(str, "NO", 2) || !strncmp(str, "SO", 2) || !strncmp(str, "WE", 2) || !strncmp(str, "EA", 2))
+			if (map_check_element_texture(str + 3) == 1)
+				return (1);
+		curr = curr->next;
+	}
+	return (0);
+}
+
 void	map_check_err(t_map *map_temp, t_list *temp, t_list *raw_map_file, char *map_str)
 {
 	t_list	*curr;
@@ -325,10 +488,14 @@ void	map_check_err(t_map *map_temp, t_list *temp, t_list *raw_map_file, char *ma
 	curr = temp;
 	while (curr != NULL && curr->content != NULL)
 	{
+		if (is_empty_line((char *)curr->content) && ((char *)curr->content)[0] != ' ')
+			printf("empty line in map\n");
 		count_players += map_check_player((char *)curr->content);
 		count_invalid_char += map_check_invalid_char((char *)curr->content);
 		curr = curr->next;
 	}
+	if (map_check_elements(raw_map_file) == 1)
+		printf("element format incorrect\n");//replace with error handler
 	if (count_players > 1 || count_players < 1)
 		printf("incorrect player count\n");
 	if (count_invalid_char > 0)
@@ -358,12 +525,103 @@ void	map_check_setup(t_list *curr, t_list *raw_map_file, char *map_str)
 	}
 	while (++index < ((map_temp.width * map_temp.height) + map_temp.width))
 		map_temp.tiles[index].type = get_tiletype(' ');
-	print_map(&map_temp);//dont need this
-	printf("\n");
+//	map_print(&map_temp);//dont need this
+//	printf("\n");
 	map_check_err(&map_temp, temp, raw_map_file, map_str);
 }
 
-t_map	map_init(t_map *map, char *map_str)
+
+
+void	remove_empty_lines(t_list **raw_map_file)
+{
+	t_list	*curr;
+	t_list	*temp;
+
+	int lines = (intptr_t)raw_map_file[0]->content;
+	*raw_map_file = raw_map_file[0]->next;
+	curr = *raw_map_file;
+	while(curr != NULL && curr->next != NULL) 
+	{
+		if (map_starting_tile((char *)curr->content) == 1)
+			break ;
+		if (is_empty_line(curr->next->content) == 1)
+		{
+			temp = curr->next;
+			curr->next = temp->next;
+			ft_lstdelone(temp, free);
+		}
+		else
+			curr = curr->next;
+	}
+}
+
+void	player_rot_setup(char rot, t_player *player)
+{
+	if (rot == 'N')
+	{
+		player->dir.x = 0;
+		player->dir.y = 1;
+	}
+	else if (rot == 'S')
+	{
+		player->dir.x = 0;
+		player->dir.y = -1;
+	}
+	else if (rot == 'E')
+	{
+		player->dir.x = 1;
+		player->dir.y = 0;
+	}
+	else if (rot == 'W')
+	{
+		player->dir.x = -1;
+		player->dir.y = 0;
+	}
+}
+
+void	player_pos_setup(t_list *curr, t_player *player)
+{
+	char	*str;
+	int	x;
+	int	y;
+
+	x = -1;
+	y = 0;
+	while (curr != NULL && curr->content != NULL)
+	{
+		str = (char *)curr->content;
+		while (str[++x] != '\n' && str[x] != '\0')
+		{
+			if (str[x] == 'N' || str[x] == 'S' || str[x] == 'E' || str[x] == 'W')
+			{
+				player_rot_setup(str[x], player);
+				player->pos.x = x;
+				player->pos.y = y;
+			}
+		}
+		y++;
+		curr = curr->next;
+	}
+}
+
+t_player	player_setup(t_list *curr, t_world *world)
+{
+	t_player player;
+
+	player_pos_setup(curr, &player);
+	player.moveSpeed = 0.05;
+	player.rotSpeed = 0.001;
+	return (player);
+}
+
+void	map_modifier_setup(t_list *raw_map_file, t_map *map, t_world *world)
+{
+
+
+//do similar checks to want we already did with the element checker but we wanna call into functions
+}
+
+t_map	map_init(t_map *map, char *map_str, t_world *world)
 {
 	t_list	*curr;
 	t_list	*raw_map_file;
@@ -371,31 +629,31 @@ t_map	map_init(t_map *map, char *map_str)
 
 	raw_map_file = ft_lst_readfile(map_str);
 	if (raw_map_file == NULL)
-		exit(2);
+		exit(2);//returnrapper here
 	index = 0;
+	remove_empty_lines(&raw_map_file);
+	replace_tabs(raw_map_file);
 	curr = raw_map_file;
-	int lines = (intptr_t)curr->content;
-	curr = curr->next;
-	replace_tabs(curr);
-	while (curr != NULL && map_starting_point((char *)curr->content) == 1)
+	while (curr != NULL && map_skip_over_modifiers((char *)curr->content) == 1)
 		curr = curr->next;
 	map_check_setup(curr, raw_map_file, map_str);
 	map->width = map_width_size(curr);
 	map->height = map_height_size(curr);
+	world->player = player_setup(curr, world);
 	while (curr != NULL && curr->content != NULL)
 	{
 		index += map_tiles(map, (char *)curr->content, index);
-		printf("%s", (char *)curr->content);
 		curr = curr->next;
 	}
+	map_modifier_setup(raw_map_file, map, world);
+	//free raw_map_file
 	return (*map);
 }
 
-t_map	map_parse(int argc, char **argv)//get the map done first
+t_map	map_parse(int argc, char **argv, t_world *world)
 {
 	t_map	map;
 	char	*map_str;
-
 
 	if (argc == 1)
 		map_str = "map1.cub";
@@ -403,18 +661,16 @@ t_map	map_parse(int argc, char **argv)//get the map done first
 		map_str = argv[1];
 	else
 		exit(2);
-	map = map_init(&map, map_str);
+	map = map_init(&map, map_str, world);
 	return (map);
 }
 
-t_world	world_preset(int argc, char **argv)
+t_world	world_preset(int argc, char **argv, t_world *world)
 {
-	t_world	world;
+	world->map = map_parse(argc, argv, world);
+	
 
-	world.map = map_parse(argc, argv);
-	print_map(&world.map);
-
-	return (world);
+	return (*world);
 }
 
 int	main(int argc, char **argv)
@@ -427,7 +683,9 @@ int	main(int argc, char **argv)
 	printf("BEGIN\n\n");
 
 	t_world	world;
-	world = world_preset(argc, argv);
+	world = world_preset(argc, argv, &world);
+//	map_print(&world.map);
+//	printf("\n%f\n", world.player.moveSpeed);
 	return (0);
 
 
@@ -437,3 +695,36 @@ int	main(int argc, char **argv)
 //	mlx_put_image_to_window(mlx, win, tex.img, 0 ,0);
 //	mlx_loop(mlx);
 }
+
+/*
+t_map	map_init(t_map *map, char *map_str)
+{
+	t_list	*curr;
+	t_list	*raw_map_file;
+	int		index;
+
+	raw_map_file = ft_lst_readfile(map_str);
+	if (raw_map_file == NULL)
+		exit(2);//returnrapper here
+	index = 0;
+	curr = raw_map_file;
+	int lines = (intptr_t)curr->content;
+	curr = curr->next;
+	remove_empty_lines(&raw_map_file);
+	replace_tabs(curr);
+	while (curr != NULL && map_starting_tile((char *)curr->content) == 1)
+		curr = curr->next;
+	map_check_setup(curr, raw_map_file, map_str);
+	map->width = map_width_size(curr);
+	map->height = map_height_size(curr);
+	while (curr != NULL && curr->content != NULL)
+	{
+		index += map_tiles(map, (char *)curr->content, index);
+		printf("%s", (char *)curr->content);// dont need this
+		curr = curr->next;
+	}
+	return (*map);
+}
+*/
+//	printf("%s", (char *)curr->content);// dont need this
+
