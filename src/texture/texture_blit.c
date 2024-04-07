058a6f213c27a05c60567625e4ffeda8a64f5f34
Christopher Lovell <clovell@student.42adel.org.au>
2024-04-07 16:30:14 +0930

    Update texture_blit to loop to the minimum width/height

--- src/texture/texture_blit.c
+++ src/texture/texture_blit.c
@@ -6,12 +6,15 @@
 /*   By: clovell <clovell@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 20:39:14 by clovell           #+#    #+#             */
-/*   Updated: 2024/02/20 17:48:29 by marvin           ###   ########.fr       */
+/*   Updated: 2024/04/07 16:28:56 by clovell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <math.h>
 #include "texture.h"
 #include "rect.h"
+#include <math.h>
+#include "clmath.h"
+#include <sys/param.h>
 
 /* Copies pixel data from one texture to another, minimally. */
 void	texture_blit_rect(t_texture *dst, t_texture *src, t_rect area)
@@ -20,13 +23,12 @@ void	texture_blit_rect(t_texture *dst, t_texture *src, t_rect area)
 	int	i;
 	int	max_j;
 	int	max_i;
-
-	max_j = fmin(area.max.y, fmin(src->height, dst->height));
-	max_i = fmin(area.max.x, fmin(src->width, dst->width));
-	j = fmin(0, area.min.y) - 1;
+	max_j = MIN(area.max.y, MIN(src->height, dst->height));
+	max_i = MIN(area.max.x, MIN(src->width, dst->width));
+	j = MIN(0, area.min.y) - 1;
 	while (++j < max_j)
 	{
-		i = fmin(0, area.min.x) - 1;
+		i = MIN(0, area.min.x) - 1;
 		while (++i < area.max.x)
 		{
 			pixel_set(*dst, i, j, pixel_get(*src, i, j));
@@ -40,12 +42,16 @@ void	texture_blit(t_texture src, t_texture dst, t_vec2 pos)
 	int	x;
 	int	y;
 	int	col;
+	int	min_x;
+	int	min_y;
 
+	min_x = MIN(dst.width, src.width);
+	min_y = MIN(dst.height, src.height);
 	y = -1;
-	while (++y < src.height)
+	while (++y < min_y)
 	{
 		x = -1;
-		while (++x < src.width)
+		while (++x < min_x)
 		{
 			col = colour_blend(pixel_get_s(src, x, y), \
 				pixel_get_s(dst, pos.x + x, pos.y + y));
