be43f1b0f12891d9afa4366717e9f24809768fde
Christopher Lovell <chris.lovellmail@gmail.com>
2024-04-16 08:57:46 +0930

    Update floor and ceiling colour modifiers to use ft_sescanf

--- src/modifiers/mod_func_floor_ceiling_color.c
+++ src/modifiers/mod_func_floor_ceiling_color.c
@@ -6,30 +6,41 @@
 /*   By: clovell <clovell@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 15:22:55 by kmordaun          #+#    #+#             */
-/*   Updated: 2024/03/16 08:28:45 by clovell          ###   ########.fr       */
+/*   Updated: 2024/04/12 16:04:20 by clovell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 #include "render.h"
+typedef union u_colour
+{
+	int	rgba;
+	char channels[4];
+} t_ucolour;
+
 /*
- * used for floor colour in RGB seperated by a colon "F 255,255,10"
+ * used for floor colour in RGB seperated by a colon "F 123,10,13"
  */
 t_err	mod_gen_f(char *content, int index, t_world *world, t_map *map)
 {
-	char	**f;
-
-	(void)world;
-	map->mods[index].type = MT_FLOOR_COLOR;
-	ft_strlcpy(map->mods[index].content, content, MOD_CONTENT_MAX);
-	f = ft_split(content, ',');
-	map->use_floor = true;
-	map->color_floor = 0;
-	map->color_floor |= ft_atoi(f[0]) << OF_RED;
-	map->color_floor |= ft_atoi(f[1]) << OF_GREEN;
-	map->color_floor |= ft_atoi(f[2]) << OF_BLUE;
-	map->color_floor |= R_ALPHA;
-	free_str_array(f);
+	int			r;
+	int			g;
+	int			b;
+	t_ucolour	colour;
+	const int	found = ft_sescanf(content, "%u,%u,%u\v", &r, &g, &b);
+	static bool stop = false;
+	if (stop)
+		return 0;
+	stop = true;
+	if (found != 3 && r >= 256 && g >= 256 && b >= 256)
+		return (1);
+	colour.channels[0] = b;
+	colour.channels[1] = g;
+	colour.channels[2] = r;
+	colour.channels[3] = R_ALPHA;
+	world->map.color_floor = colour.rgba;
+	world->map.use_floor = true;
+	printf("floorin:%x\n", colour.rgba);
 	return (0);
 }
 
@@ -38,18 +49,23 @@ t_err	mod_gen_f(char *content, int index, t_world *world, t_map *map)
  */
 t_err	mod_gen_c(char *content, int index, t_world *world, t_map *map)
 {
-	char	**c;
-
-	(void)world;
-	map->mods[index].type = MT_CEILING_COLOR;
-	ft_strlcpy(map->mods[index].content, content, MOD_CONTENT_MAX);
-	c = ft_split(content, ',');
-	map->use_ceiling = true;
-	map->color_ceiling = 0;
-	map->color_ceiling |= ft_atoi(c[0]) << OF_RED;
-	map->color_ceiling |= ft_atoi(c[1]) << OF_GREEN;
-	map->color_ceiling |= ft_atoi(c[2]) << OF_BLUE;
-	map->color_ceiling |= R_ALPHA;
-	free_str_array(c);
+	int			r;
+	int			g;
+	int			b;
+	t_ucolour	colour;
+	const int	found = ft_sescanf(content, "%u,%u,%u\v", &r, &g, &b);
+	static bool stop = false;
+	if (stop)
+		return 0;
+	stop = true;
+	if (found != 3 && r >= 256 && g >= 256 && b >= 256)
+		return (1);
+	colour.channels[0] = b;
+	colour.channels[1] = g;
+	colour.channels[2] = r;
+	colour.channels[3] = R_ALPHA;
+	printf("cieling:%x\n", colour.rgba);
+	world->map.color_ceiling = colour.rgba;
+	world->map.use_ceiling = true;
 	return (0);
 }
