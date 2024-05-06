f3adbe0af96c1b71ceb7bf4fc67caaafb067348f
Christopher Lovell <chris.lovellmail@gmail.com>
2024-05-06 23:12:24 +0930

    norm

--- inc/tasks/tutorial.h
+++ inc/tasks/tutorial.h
@@ -6,11 +6,21 @@
 /*   By: clovell <clovell@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 18:58:59 by clovell           #+#    #+#             */
-/*   Updated: 2024/05/06 23:08:01 by clovell          ###   ########.fr       */
+/*   Updated: 2024/05/06 23:12:16 by clovell          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "rect.h"
 
+typedef enum e_iter_func_ctrl
+{
+	ITER_STOP,
+	ITER_CONTINUE,
+	ITER_SKIP_NEXT,
+	ITER_REPEAT,
+	ITER_ERR,
+}	t_iter_func_ctrl;
+
+typedef t_iter_func_ctrl		(*t_iter_func)(void *element, void *ctx);
 typedef struct s_task_tip		t_task_tip;
 
 struct	s_task_tip
@@ -32,17 +42,6 @@ struct	s_task_tutorial
 	char		**active_split;
 	int			active_len;
 };
-
-typedef enum e_iter_func_ctrl
-{
-	ITER_STOP,
-	ITER_CONTINUE,
-	ITER_SKIP_NEXT,
-	ITER_REPEAT,
-	ITER_ERR,
-}	t_iter_func_ctrl;
-
-typedef t_iter_func_ctrl	(*t_iter_func)(void *element, void *ctx);
 
 int		task_tut_instance(t_task_tutorial *new, t_task_tutorial *prefab);
 
